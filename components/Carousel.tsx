d788c2ea4e2e74bb2a8a0af4652eda1b0dafaeea
Ahmed Elnemr <elnemr21092003@gmail.com>
2025-12-01 20:17:24 +0200

    edit carousel layout

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -11,16 +11,24 @@ import Image from "next/image"
 
 export function CarouselDemo({products}: any) {
     return (
-        <Carousel className="w-full max-w-xs mx-auto my-[50px]">
-            <CarouselContent>
-                {Array.from({ length: 5 }).map((_, index) => (
-                <CarouselItem key={index} className="h-full">
-                    <Image src={products.data[index].images[0]} alt={`Product image ${index + 1}`} width={300} height={300}/>
-                </CarouselItem>
-                ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-        </Carousel>
+        <div className="slinding">
+            <div className="container grid-container my-[50px]">
+                <div className="text flex flex-col justify-center mb-8 text-center md:text-start ">
+                    <h2 className="text-2xl font-bold mb-4">Featured Products</h2>
+                    <p className="text-gray-600">Explore our exclusive collection of featured products curated just for you.</p>
+                </div>
+                <Carousel className="w-full max-w-xs mx-auto">
+                    <CarouselContent>
+                        {Array.from({ length: 5 }).map((_, index) => (
+                        <CarouselItem key={index} className="h-full">
+                            <Image src={products.data[index].images[0]} alt={`Product image ${index + 1}`} width={300} height={300}/>
+                        </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
+            </div>
+        </div>
     )
 }
