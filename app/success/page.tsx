16c931f3b0cf6f092c4232d1432f66bccdda5ce4
Ahmed Elnemr <elnemr21092003@gmail.com>
2025-12-01 18:08:04 +0200

    edit redirect link after payment proccess

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -11,7 +11,7 @@ const Success = () => {
     }, [clearCart])
     return (
         <div className='success'>
-            <div className="container flex flex-col items-center justify-center h-[90vh]">
+            <div className="container flex flex-col items-center justify-center h-[80vh]">
                 <h1 className="text-3xl font-bold mb-4 text-green-500">Payment Successful!</h1>
                 <p className="text-lg">Thank you for your purchase. Your payment has been processed successfully.</p>
                 <Button className='bg-green-500 hover:bg-green-500 mt-8'><Link href={'/products'}>continue shopping</Link></Button>
